b02a7cfc300abf8df4e3899cf2772cbcacee4434
Bayarjargal <36721957+BayraaKaspersky@users.noreply.github.com>
2026-04-01 13:00:24 +0900

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.js
@@ -0,0 +1,31 @@
+class Solution {
+    encode(strs) {
+        let encoded = ""
+        for (let str of strs) {
+            encoded += str.length + "#" + str
+        }
+        return encoded
+    }
+
+    decode(str) {
+        let decoded = []
+        let i = 0
+
+        while (i < str.length) {
+            let j = i
+
+            while (str[j] !== "#") {
+                j++
+            }
+
+            let length = parseInt(str.substring(i, j))
+            let word = str.substring(j + 1, j + 1 + length)
+
+            decoded.push(word)
+
+            i = j + 1 + length
+        }
+
+        return decoded
+    }
+}
